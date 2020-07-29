d9635b3f20eb05f52da102c00f68f28577897065
David Archuleta Jr <darchuletajr@gmail.com>
2020-07-29 07:25:43 -0700

    flake8 W293: blank line contains whitespace

--- cardpointeflask/blueprints/user/decorators.py
+++ cardpointeflask/blueprints/user/decorators.py
@@ -7,7 +7,7 @@ from flask_login import current_user
 def anonymous_required(url='/settings'):
     """
     Redirect a user to a specified location if they are already signed in.
-    
+
     :param url: URL to be redirected to if invalid
     :type url: str
     :return: Function
@@ -17,11 +17,11 @@ def anonymous_required(url='/settings'):
         def decorated_function(*args, **kwargs):
             if current_user.is_authenticated:
                 return redirect(url)
-            
+
             return f(*args, **kwargs)
-        
+
         return decorated_function
-    
+
     return decorator
 
 
@@ -38,9 +38,9 @@ def role_required(*roles):
             if current_user.role not in roles:
                 flash('You do not have permission to do that.', 'error')
                 return redirect('/')
-            
+
             return f(*args, **kwargs)
-        
+
         return decorated_function
-    
+
     return decorator
